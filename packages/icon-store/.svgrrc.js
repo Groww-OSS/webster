44ff1bf4eeeda7ce69cacd8bc0c3037d871bb396
Abhinab <abhinabpsc@groww.in>
2024-08-09 11:45:18 +0530

    Final Commit

--- packages/icon-store/.svgrrc.js
+++ packages/icon-store/.svgrrc.js
@@ -1,3 +1,8 @@
+const hash = require('string-hash');
+
+
+let context = __dirname;
+
 module.exports = {
   template: require("./scripts/helpers/template"),
   dimensions: false,
@@ -8,9 +13,16 @@ module.exports = {
         params: {
           overrides: {
             removeViewBox: false,
-          },
-        },
-      },
+            cleanupIds: {
+              // cleanUpIds minification seems to sometimes generate duplicate IDs.
+              // We disabled this so it now only uses the id that was in the SVG to begin with.
+              minify: false,
+              // Don't remove unused IDs, as sometimes they are applied later on via CSS.
+              remove: false
+            },
+          }
+        }
+      }
     ],
   },
   index: false,
