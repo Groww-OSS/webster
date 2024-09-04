8dd7198ba0cf803a3b6701043bc3cd8cb0eb3d9b
harshpathakzz <harshpathak2017@gmail.com>
2024-09-04 16:29:58 +0530

    refactored

--- packages/ui-toolkit/src/components/molecules/Calendar/MonthCalendar/index.tsx
+++ packages/ui-toolkit/src/components/molecules/Calendar/MonthCalendar/index.tsx
@@ -29,11 +29,13 @@ class MonthCalendar extends React.PureComponent<Props, State> {
   }
 
   // Initial state
+
   state: State = {
     dateToShow: this.props.currentDate
   };
 
   // Update state if currentDate prop changes
+
   componentDidUpdate(prevProps: Props) {
     if (prevProps.currentDate.getTime() !== this.props.currentDate.getTime()) {
       this.setState({
@@ -45,7 +47,10 @@ class MonthCalendar extends React.PureComponent<Props, State> {
 
   render() {
     const { dateToShow } = this.state;
-    const { currentDate, minMonth, maxMonth } = this.props;
+    const { currentDate } = this.props;
+
+    const isPreviousYearDisabled = this.isPreviousYearDisabled();
+    const isNextYearDisabled = this.isNextYearDisabled();
 
     // Get the index of the current month
     const currentMonthIndex = currentDate.getMonth();
@@ -56,29 +61,29 @@ class MonthCalendar extends React.PureComponent<Props, State> {
           <div className="valign-wrapper mn12YearRow">
             <div
               className={
-                cn('valign-wrapper cur-po', {
-                  'contentSecondary cur-no': this.isPreviousYearDisabled()
+                cn({ 'valign-wrapper cur-po': !isPreviousYearDisabled }, {
+                  'contentSecondary cur-no': isPreviousYearDisabled
                 })
               }
-              onClick={this.handlePrevYearClick}
+              onClick={() => this.handlePrevYearClick(isPreviousYearDisabled)}
             >
               <KeyboardArrowLeft className="mn12YearIcon" />
             </div>
             <div>{dateToShow.getFullYear()}</div>
             <div
               className={
-                cn('valign-wrapper cur-po', {
-                  'contentSecondary cur-no': this.isNextYearDisabled()
+                cn({ 'valign-wrapper cur-po': !isNextYearDisabled }, {
+                  'contentSecondary cur-no': isNextYearDisabled
                 })
               }
-              onClick={this.handleForwardYearClick}
+              onClick={() => this.handleForwardYearClick(isNextYearDisabled)}
             >
               <KeyboardArrowRight className="mn12YearIcon" />
             </div>
           </div>
           <div className="valign-wrapper mn12MonthBox">
-            {
-              // Render each month
+            {              // Render each month
+
               MONTHS.map((month, index) => (
                 <div className="mn12Month"
                   key={`${dateToShow?.getTime()}${index}`}
@@ -87,13 +92,13 @@ class MonthCalendar extends React.PureComponent<Props, State> {
                     className={
                       cn('mn12MonthText valign-wrapper cur-po bodyBase', {
                         'mn12MonthTextSelected contentInversePrimary':
-                      index === currentMonthIndex &&
-                      currentDate.getFullYear() === dateToShow.getFullYear(),
-                        'mn12MonthBack':
-                      !(
                         index === currentMonthIndex &&
-                        currentDate.getFullYear() === dateToShow.getFullYear()
-                      ),
+                        currentDate.getFullYear() === dateToShow.getFullYear(),
+                        'mn12MonthBack':
+                        !(
+                          index === currentMonthIndex &&
+                          currentDate.getFullYear() === dateToShow.getFullYear()
+                        ),
                         'cc12DisableDate': this.isMonthDisabled(index)
                       })
                     }
@@ -111,6 +116,7 @@ class MonthCalendar extends React.PureComponent<Props, State> {
   }
 
   // Handle month click event
+
   onMonthClick = (index: number) => {
     const { dateToShow } = this.state;
     const newDate = new Date(dateToShow.getFullYear(), index, 1);
@@ -123,11 +129,11 @@ class MonthCalendar extends React.PureComponent<Props, State> {
     this.setState({
       dateToShow: newDate
     });
-  }
+  };
 
   // Handle previous year click event
-  handlePrevYearClick = () => {
-    if (this.isPreviousYearDisabled()) {
+  handlePrevYearClick = (isPreviousYearDisabled:boolean) => {
+    if (isPreviousYearDisabled) {
       return;
     }
 
@@ -135,11 +141,12 @@ class MonthCalendar extends React.PureComponent<Props, State> {
     const newDate = new Date(dateToShow.getFullYear() - 1, dateToShow.getMonth(), 1);
 
     this.setState({ dateToShow: newDate });
-  }
+  };
+
 
   // Handle next year click event
-  handleForwardYearClick = () => {
-    if (this.isNextYearDisabled()) {
+  handleForwardYearClick = (isNextYearDisabled:boolean) => {
+    if (isNextYearDisabled) {
       return;
     }
 
@@ -147,26 +154,25 @@ class MonthCalendar extends React.PureComponent<Props, State> {
     const newDate = new Date(dateToShow.getFullYear() + 1, dateToShow.getMonth(), 1);
 
     this.setState({ dateToShow: newDate });
-  }
+  };
 
   // Check if a month is disabled based on minMonth and maxMonth props
+
   isMonthDisabled = (monthIndex: number): boolean => {
     const { dateToShow } = this.state;
     const { minMonth, maxMonth } = this.props;
     const currentDate = new Date(dateToShow.getFullYear(), monthIndex, 1);
 
-    if (minMonth && this.compareMonth(minMonth, currentDate)) {
-      return true;
-    }
-
-    if (maxMonth && this.compareMonth(currentDate, maxMonth)) {
+    if ((this.compareMonth(minMonth, currentDate)) ||
+          (this.compareMonth(currentDate, maxMonth))) {
       return true;
     }
 
     return false;
-  }
+  };
 
   // Check if the previous year button should be disabled
+
   isPreviousYearDisabled = (): boolean => {
     const { dateToShow } = this.state;
     const { minMonth } = this.props;
@@ -176,9 +182,10 @@ class MonthCalendar extends React.PureComponent<Props, State> {
     }
 
     return false;
-  }
+  };
 
   // Check if the next year button should be disabled
+
   isNextYearDisabled = (): boolean => {
     const { dateToShow } = this.state;
     const { maxMonth } = this.props;
@@ -188,15 +195,17 @@ class MonthCalendar extends React.PureComponent<Props, State> {
     }
 
     return false;
-  }
+  };
 
   // Compare two dates by year and month
-  compareMonth = (dateOne: Date, dateTwo: Date): boolean => {
+
+  compareMonth = (dateOne: Date | null | undefined, dateTwo: Date | null | undefined): boolean => {
+    if (!dateOne || !dateTwo) return false;
     const yearDiff = dateOne.getFullYear() - dateTwo.getFullYear();
 
     if (yearDiff !== 0) return yearDiff > 0;
     return dateOne.getMonth() > dateTwo.getMonth();
-  }
+  };
 }
 
 
@@ -205,11 +214,11 @@ type Props = {
   onDateChange: (date: Date) => void;
   minMonth?: Date | null;
   maxMonth?: Date | null;
-}
+};
 
 
 type State = {
   dateToShow: Date;
-}
+};
 
 export default MonthCalendar;
