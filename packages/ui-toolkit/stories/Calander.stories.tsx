d651045612f8d60e7b88abddac7c5664baedf17a
harshpathakzz <harshpathak2017@gmail.com>
2024-09-10 00:39:30 +0530

    added min max props

--- packages/ui-toolkit/stories/Calander.stories.tsx
+++ packages/ui-toolkit/stories/Calander.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import { StoryFn } from '@storybook/react';
-
 import { Calendar } from '../src/components/molecules';
 import { Props as CalendarProps, CALENDAR_TYPE } from '../src/components/molecules/Calendar/Calendar';
 
@@ -12,20 +10,51 @@ export default {
 };
 
 
-const Template: StoryFn<CalendarProps> = (args) => (
-  <Calendar {...args} />
-);
+const Template: StoryFn<CalendarProps> = (args) => <Calendar {...args} />;
 
-export const Month = {
-  render: Template,
-  args: {
-    type: CALENDAR_TYPE.MONTH
-  }
-};
+export const CalendarStory = {
+  render: (args) => {
+    const updatedArgs = { ...args };
 
-export const Date = {
-  render: Template,
+    // Adjust props based on the selected type (MONTH or DATE)
+    if (args.type === CALENDAR_TYPE.MONTH) {
+      updatedArgs.minMonth = new Date(args.minMonth);
+      updatedArgs.maxMonth = new Date(args.maxMonth);
+
+    } else if (args.type === CALENDAR_TYPE.DATE) {
+      updatedArgs.minDate = new Date(args.minDate);
+      updatedArgs.maxDate = new Date(args.maxDate);
+    }
+
+    return <Template {...updatedArgs} />;
+  },
   args: {
-    type: CALENDAR_TYPE.DATE
+    type: CALENDAR_TYPE.MONTH,
+    minMonth: new Date(2021, 6, 6),
+    maxMonth: new Date(2031, 9, 6),
+    minDate: new Date(2021, 6, 6),
+    maxDate: new Date(2031, 6, 20)
+  },
+  argTypes: {
+    type: {
+      control: 'select',
+      options: Object.values(CALENDAR_TYPE)
+    },
+    minMonth: {
+      control: 'date',
+      if: { arg: 'type', eq: CALENDAR_TYPE.MONTH } // Only show when MONTH type is selected
+    },
+    maxMonth: {
+      control: 'date',
+      if: { arg: 'type', eq: CALENDAR_TYPE.MONTH } // Only show when MONTH type is selected
+    },
+    minDate: {
+      control: 'date',
+      if: { arg: 'type', eq: CALENDAR_TYPE.DATE } // Only show when DATE type is selected
+    },
+    maxDate: {
+      control: 'date',
+      if: { arg: 'type', eq: CALENDAR_TYPE.DATE } // Only show when DATE type is selected
+    }
   }
 };
