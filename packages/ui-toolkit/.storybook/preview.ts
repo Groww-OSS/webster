3bbaa5570ed76bb6643c27590706ec49bc7ff9c1
Abhinab Pratap Singh Chauhan <144772429+abhinabgroww@users.noreply.github.com>
2024-09-12 18:08:25 +0530

    Testing

--- packages/ui-toolkit/.storybook/preview.ts
+++ packages/ui-toolkit/.storybook/preview.ts
@@ -1,5 +1,5 @@
 import type { Preview } from "@storybook/react";
-import "@groww-tech/mint-css/dist/index.css";
+import "../node_modules/@groww-tech/mint-css/dist/index.css";
 
 const preview: Preview = {
   parameters: {
