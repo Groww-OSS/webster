2c3e69765e9c3dceb30ebc8029a45bf83a04c118
Abhinab <abhinabpsc@groww.in>
2024-09-16 14:50:15 +0530

    Removed unwanted commits

--- packages/ui-toolkit/.storybook/main.ts
+++ packages/ui-toolkit/.storybook/main.ts
@@ -22,6 +22,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-styling-webpack")
   ],
 
   framework: {
@@ -31,6 +32,8 @@ const config: StorybookConfig = {
 
   core: {
     disableWhatsNewNotifications: true
-  }
+  },
+  
+  
 };
 export default config;
