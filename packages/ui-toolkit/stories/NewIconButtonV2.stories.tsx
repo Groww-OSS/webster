0bc56cf6344e7d64659088a129e3412a05a0b8e0
harshpathakzz <harshpathak2017@gmail.com>
2024-12-21 23:25:53 +0530

    added story

---
+++ packages/ui-toolkit/stories/NewIconButtonV2.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StoryFn } from '@storybook/react';
+import IconButtonV2, { IconButtonProps } from '../src/components/atoms/newIconButtonV2/IconButtonV2';
+import { MdsIcAddPlus } from '@groww-tech/icon-store/mint-icons';
+
+export default {
+  title: 'IconButtonV2',
+  component: IconButtonV2,
+  argTypes: {
+    disabled: { control: 'boolean' },
+    onClick: { action: 'clicked' }
+  },
+  tags: [ 'autodocs' ]
+};
+
+
+const Template: StoryFn<IconButtonProps> = (args) => (
+  <IconButtonV2 {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  Icon: MdsIcAddPlus,
+  disabled: false
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  Icon: MdsIcAddPlus,
+  disabled: true
+};
