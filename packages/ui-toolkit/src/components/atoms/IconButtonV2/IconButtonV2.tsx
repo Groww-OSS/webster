fdfae439b2dd7071cf1b7d39d5102c5fc9616e2b
harshpathakzz <harshpathak2017@gmail.com>
2024-12-24 17:26:35 +0530

    updated css

--- packages/ui-toolkit/src/components/atoms/IconButtonV2/IconButtonV2.tsx
+++ packages/ui-toolkit/src/components/atoms/IconButtonV2/IconButtonV2.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { ReactIconComponentType } from '@groww-tech/icon-store/types.d';
+import { MdsIcSpinnerLoader } from '@groww-tech/icon-store/mint-icons';
+import cn from 'classnames';
 import './styles/index.css';
 
 export type IconButtonProps = {
   onClick: () => void;
   Icon: ReactIconComponentType;
   disabled?: boolean;
-  loading?: boolean;
+  isLoading?: boolean;
   size?: 'sm' | 'md' | 'lg';
   isCompact?: boolean;
   shape?: 'circle' | 'square';
-  outline?: boolean;
-  // selectedColor?: 'neutral' | 'accent';
 };
 
 
@@ -19,22 +19,50 @@ const IconButtonV2: React.FC<IconButtonProps> = ({
   onClick,
   Icon,
   disabled = false,
-  loading = false,
+  isLoading = false,
   size = 'md',
   isCompact = false,
-  shape = 'circle',
-  outline = false
-  // selectedColor = 'neutral',
+  shape
 }) => {
+  const iconBtnContainerClass = cn({
+    [`icon-btn-container-${size}-compact`]: isCompact && !shape,
+    [`icon-btn-container-${size}-${shape}`]: shape,
+    [`icon-btn-container-${size}`]: !isCompact && !shape
+  });
+
+  // Class for button border based on shape and disabled state
+  const iconBtnBorderClass = cn({
+    ['icon-btn-border-none']: !shape || (shape && disabled),
+    ['borderPrimary']: shape && !disabled
+  });
+
+  // Placeholder for background class logic
+  const iconBtnBackgroundClass = cn({
+    ['icon-btn-background-disabled']: disabled && shape,
+    ['backgroundTransparentHover']: !disabled
+  });
+
+  const iconBtnColorClass = cn({
+    ['contentDisabled']: disabled,
+    ['contentprimary']: !disabled
+  });
 
   return (
     <button
       onClick={onClick}
       disabled={disabled}
+      className={cn(iconBtnContainerClass, iconBtnBorderClass, iconBtnBackgroundClass, iconBtnColorClass)}
     >
-      <div>
-        <Icon />
-      </div>
+      {
+        isLoading ? (
+          <MdsIcSpinnerLoader
+            size={20}
+            className="icon-spinner"
+          />
+        ) : (
+          <Icon size={20} />
+        )
+      }
     </button>
   );
 };
