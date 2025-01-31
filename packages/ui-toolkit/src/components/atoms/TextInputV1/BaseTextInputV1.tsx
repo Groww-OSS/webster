c25946f78cbeff1d81ac83c1524b5e5a298bd3d5
harshpathakzz <harshpathak2017@gmail.com>
2025-01-31 16:02:54 +0530

    added check for body token

--- packages/ui-toolkit/src/components/atoms/TextInputV1/BaseTextInputV1.tsx
+++ packages/ui-toolkit/src/components/atoms/TextInputV1/BaseTextInputV1.tsx
@@ -3,6 +3,11 @@ import './styles.css';
 
 import { TextInputProps } from './TextInputV1';
 
+const bodyClasses = [
+  'bodySmall', 'bodySmallHeavy', 'bodyBase', 'bodyBaseHeavy',
+  'bodyLarge', 'bodyLargeHeavy', 'bodyXLarge', 'bodyXLargeHeavy'
+];
+
 const BaseTextInputV1 = React.forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
   const {
     onKeyDown = () => { },
@@ -17,8 +22,12 @@ const BaseTextInputV1 = React.forwardRef<HTMLInputElement, TextInputProps>((prop
     errorDataTestId = '',
     PrefixComponent,
     SuffixComponent,
+    className = '',
     ...rest
   } = props;
+
+  const shouldApplyCalculatedClass = !bodyClasses.some(bodyClass => className.includes(bodyClass));
+  const computedClass = shouldApplyCalculatedClass ? calculateInputClass(size) : '';
 
   return (
     <>
@@ -29,7 +38,7 @@ const BaseTextInputV1 = React.forwardRef<HTMLInputElement, TextInputProps>((prop
           <input
             ref={ref}
             data-test-id={inputDataTestId.length ? inputDataTestId : null}
-            className={`text-input-v1-primary-input ${calculateInputClass(size)} ${variant}`}
+            className={`text-input-v1-primary-input ${className} ${computedClass} ${variant}`}
             onCopy={onCopy}
             onPaste={onPaste}
             onKeyUp={onKeyUp}
